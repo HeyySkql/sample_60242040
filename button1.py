93d6ae00ab8a99a17e4cb1421c30177719e40de9
HeyySkql <skql4728@gmail.com>
2025-09-14 18:44:03 +0900

    Add files via upload

---
+++ button1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window =Tk()
+
+b1 = Button(window, text="Nakyung Lee")
+b2 = Button(window, text="60242040")
+b1.grid(row=0, column=0)
+b2.grid(row=1, column=1)
+
+window.mainloop()
+
